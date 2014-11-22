e2f937b55d9fac846270c06652851b1271989b94
Joaquin Sargiotto <joaquinsargiotto@gmail.com>
2014-11-22 12:04:47 -0300

    Refactoring expandable element code.

---
+++ restman/js/ui/expandables.js
@@ -0,0 +1,29 @@
+/*
+ * This file is part of RESTMan for Opera Extension.
+ * https://github.com/jsargiot/restman
+ *
+ * Free to use under the MIT license.
+ * https://raw.githubusercontent.com/jsargiot/restman/master/LICENSE
+ *
+ * Example:
+ *
+ *  <div class="expandable">
+ *      <h1 class="expander">Expander</h1>
+ *      <p>
+ *          ...Content...
+ *      </p>
+ *  </div>
+ */
+
+$(".expandable .expander").click(function(event) {
+    /* Act on the event */
+    var section = $(this).parent();
+    
+    if (section.hasClass("closed")) {
+        section.removeClass("closed");
+    } else {
+        section.addClass("closed");
+    }
+    // Avoid going to href when coming from a link
+    return false;
+});
