2c9d2c8f41aab001697bb6eabe1570146e7c54ce
Joaquin Sargiotto <joaquin@scrapinghub.com>
2015-04-22 22:55:32 -0300

    Several UI improvements. Material design.

---
+++ restman/js/restman/body.js
@@ -0,0 +1,19 @@
+/*
+ * This file is part of RESTMan for Opera Extension.
+ * https://github.com/jsargiot/restman
+ *
+ * Free to use under the MIT license.
+ * https://raw.githubusercontent.com/jsargiot/restman/master/LICENSE
+ *
+ * This module provides functionality to manage the request's headers.
+ */
+
+var restman = restman || {};
+
+(function() {
+    'use strict';
+
+    restman.body = {
+
+    };
+})();
---
+++ restman/js/restman/variables.js
@@ -0,0 +1,20 @@
+/*
+ * This file is part of RESTMan for Opera Extension.
+ * https://github.com/jsargiot/restman
+ *
+ * Free to use under the MIT license.
+ * https://raw.githubusercontent.com/jsargiot/restman/master/LICENSE
+ *
+ * This module provides functionality to replace specific ${token} by some
+ * value.
+ */
+
+var restman = restman || {};
+
+(function() {
+    'use strict';
+
+    restman.variables = {
+
+    };
+})();
