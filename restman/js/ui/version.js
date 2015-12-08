97bcbbf36a90fa65871e4a50b31bd1fda3bd822e
Joaquin Sargiotto <joaquin@scrapinghub.com>
2015-12-07 22:15:39 -0300

    Showing version in extension main window.

---
+++ restman/js/ui/version.js
@@ -0,0 +1,10 @@
+/*
+ * This file is part of RESTMan for Opera Extension.
+ * https://github.com/jsargiot/restman
+ *
+ * Free to use under the MIT license.
+ * https://raw.githubusercontent.com/jsargiot/restman/master/LICENSE
+ */
+$(document).ready(function(event) {
+    $('#VersionNumber').text("v" + chrome.app.getDetails()['version']);
+});
